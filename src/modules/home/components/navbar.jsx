431d63f44ff250443496db4874ae1b22ba623b8d
Ahmad Anas <102130991+iamAnas-zzx@users.noreply.github.com>
2026-07-20 22:27:29 +0530

    04-Header, prisma-adapter-fix

---
+++ src/modules/home/components/navbar.jsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import Image from "next/image";
+import { SignIn, SignedOut, SignInButton, SignUpButton, SignedIn, UserButton } from '@clerk/nextjs';
+import { Button } from "@/components/ui/button";
+
+const Navbar = () => {
+    return (
+        <div className="p-4 bg-transparent fixed top-0 left-0 right-0 z-50 transition-all 
+        duration-200 border-b border-transparent">
+            <div className="max-w-5xl mx-auto w-full flex justify-between items-center" >
+                <Link href={"/"} className="flex items-center gap-2">
+                    <Image src={"/v0-logo.svg"} alt='Vibe' width={32} height={32}
+                        className="shrink-0 invert dark:invert-0" />
+                </Link>
+                <SignedOut >
+                    <div className="flex gap-2">
+                        <SignInButton>
+                            <Button variant={"outline"} size={"sm"}>
+                                Sign In
+                            </Button>
+                        </SignInButton>
+                        <SignUpButton>
+                            <Button size={"sm"}>
+                                Sign Up
+                            </Button>
+                        </SignUpButton>
+                    </div>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton/>
+                </SignedIn>
+            </div>
+        </div>
+    )
+}
+export default Navbar;
